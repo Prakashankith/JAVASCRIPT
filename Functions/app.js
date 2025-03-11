b0c080fce146dbd03befbe224699d43630d55c0c
Prakashankith <sirankithbp1999@gmail.com>
2025-03-11 18:27:55 +0530

    added a initial commit

--- Functions/app.js
+++ Functions/app.js
@@ -1,24 +1,24 @@
-let name = {
-    firstName: "ankith",
-    lastName: "sanjay",
+// let name = {
+//     firstName: "ankith",
+//     lastName: "sanjay",
 
-}
-let myName = function (hometown, city) {
-    console.log(this.firstName + " " + this.lastName + " from " + hometown + " " + city);
-}
-// myName.call(name, "Bangalore", "Mangalore")
-myName.apply(name, ["raichur"])
-let name2 = {
-    firstName: "mayank",
-    lastName: "Jspider"
-}
-// myName.call(name2, "Raichur")
+// }
+// let myName = function (hometown, city) {
+//     console.log(this.firstName + " " + this.lastName + " from " + hometown + " " + city);
+// }
+// // myName.call(name, "Bangalore", "Mangalore")
+// myName.apply(name, ["raichur"])
+// let name2 = {
+//     firstName: "mayank",
+//     lastName: "Jspider"
+// }
+// // myName.call(name2, "Raichur")
 
-// ?bind method
+// // ?bind method
 
-let printMyName = myName.bind(name2, "mumbai", "Thane")
-console.log(printMyName);
-printMyName()
+// let printMyName = myName.bind(name2, "mumbai", "Thane")
+// console.log(printMyName);
+// printMyName()
 
 // ?==========================================================================================
 //  !throttling and debouncing in javascript
@@ -64,37 +64,68 @@ printMyName()
 //     throlledCount()
 // })
 
-const btn = document.querySelector(".increment-btn")
-const btnPress = document.querySelector(".increment-pressed")
-const count = document.querySelector(".increment-count")
+// const btn = document.querySelector(".increment-btn")
+// const btnPress = document.querySelector(".increment-pressed")
+// const count = document.querySelector(".increment-count")
 
-let triggeredCount = 0;
-let pressedCount = 0;
+// let triggeredCount = 0;
+// let pressedCount = 0;
 
-const start = new Date().getTime()
-const mythrottle = (cb, d) => {
-    let last = 0;
-    return (...args) => {
-        let now = new Date().getTime();
-        if (now - last < d) return;
-        last = now;
-        return cb(...args)
-    }
+// const start = new Date().getTime()
+// const mythrottle = (cb, d) => {
+//     let last = 0;
+//     return (...args) => {
+//         let now = new Date().getTime();
+//         if (now - last < d) return;
+//         last = now;
+//         return cb(...args)
+//     }
+// }
+
+
+// const throttled = mythrottle(() => {
+//     const now = new Date().getTime()
+//     console.log(now - start);
+//     count.innerHTML = ++triggeredCount
+
+
+// }, 800)
+
+// btn.addEventListener("click", () => {
+//     btnPress.innerHTML = ++pressedCount
+
+
+// })
+
+
+function abc() {
+    console.log(abc.xyz);
 }
+abc()
+abc.xyz = 100
+abc.xyz = 200
+abc()
+console.log(typeof typeof 100);
+const arr = [...'Praveen']
+console.log(arr);
+
+console.log(parseInt('10-2'));
+console.log(parseInt('7FM'));
+console.log(parseInt('M7F'));
+
+console.log([1, 2].map(num => {
+    if (num > 0) return;
+    return num * 2
+}));
+
+console.log([] + {});
+console.log({} + []);
 
 
-const throttled = mythrottle(() => {
-    const now = new Date().getTime()
-    console.log(now - start);
-    count.innerHTML = ++triggeredCount
-
-
-}, 800)
-
-btn.addEventListener("click", () => {
-    btnPress.innerHTML = ++pressedCount
-    
-
-})
-
-
+let count = 0
+const inc = () => count++
+const log = () => console.log(count);
+inc()
+setTimeout(log, 0);
+inc();
+console.log(count);
